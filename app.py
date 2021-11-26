9884cb6ecb6d3c4ed0795598821fb200c2cdc867
Khoa Luu <khoa.luu@paradox.ai>
2021-11-26 18:01:55 +0700

    Fix deprecated default_value param

--- app.py
+++ app.py
@@ -1,8 +1,13 @@
+import os
+
 from flask import Flask
 from UnleashClient import UnleashClient
 
 
-client = UnleashClient("https://ol-unleash.herokuapp.com/api", "Unleash PoC")
+token = os.getenv("CLIENT_ACCESS_TOKEN", default=None)
+headers = {'Authorization': token} if token else {}
+
+client = UnleashClient("https://ol-unleash.herokuapp.com/api", "Unleash PoC", custom_headers=headers)
 client.initialize_client()
 
 app = Flask(__name__)
@@ -10,7 +15,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    if client.is_enabled("index-hello", default_value=True):
+    # NOTE: param `default_value` is deprecated! Use the fallback_function on the main is_enabled() method.
+    if client.is_enabled("index-hello"):
         return "Hello World!"
     else:
         return "Goodbye World!"
