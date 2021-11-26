c275d165e43127ca0a326c9edff338bf09a8d618
Khoa Luu <khoa.luu@paradox.ai>
2021-11-26 13:27:42 +0700

    Add first feature flag

--- app.py
+++ app.py
@@ -1,7 +1,23 @@
 from flask import Flask
+from UnleashClient import UnleashClient
+
+
+client = UnleashClient("https://ol-unleash.herokuapp.com/api", "Unleash PoC")
+client.initialize_client()
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def index():
-    return "Hello World!"
+    if client.is_enabled("index-hello", default_value=True):
+        return "Hello World!"
+    else:
+        return "Goodbye World!"
+
+
+if __name__ == '__main__':
+    try:
+        app.run()
+    finally:
+        client.destroy()
